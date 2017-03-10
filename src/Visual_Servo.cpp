942d80184be58bd219e50025f7864e70b692a87c
Yoga <yogaanna@gmail.com>
2017-03-09 19:37:20 -0500

    Relatively safe changes. No logic changes

--- src/Visual_Servo.cpp
+++ src/Visual_Servo.cpp
@@ -8,6 +8,9 @@ float Visual_Servo::calculate_distance(float x, float y, float x_center, float y
 	float xdiff = x-x_center;
 	float ydiff = y-y_center;
 	float dist = sqrt((xdiff*xdiff)+(ydiff*ydiff));
+	if(ydiff>0)
+		return -dist;
+	return dist;
 }
 
 float Visual_Servo::calculate_angle(float x, float y, float x_center, float y_center, float init_angle)
